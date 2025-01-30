590f405c6c470e76e4aae31091f8cf43ace9016d
KartavyaShrivastav <kartavyashrivastav28@gmail.com>
2025-01-30 11:47:04 +0530

    Solved using stack by pushin opening brackets '(' and popping closing brackets ')' while maintaing -1 as base case

---
+++ 32_Longest_Valid_Parentheses.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> st;
+        st.push(-1);
+        int maxL = 0;
+
+        for(int i=0; i<s.length(); i++){
+            if(s[i] == '('){
+                st.push(i);
+            }
+            else{
+                st.pop();
+                if(st.empty()){
+                    st.push(i);
+                }
+                else{
+                    maxL = max(maxL, i - st.top());
+                }
+            }
+        }
+
+        return maxL;
+    }
+};

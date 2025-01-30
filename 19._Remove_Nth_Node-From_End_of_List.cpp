57806e609d3de5dce263b810e6dd1e698a0a622a
KartavyaShrivastav <kartavyashrivastav28@gmail.com>
2025-01-30 09:49:11 +0530

    Solved by travering to the nth node using while loop and then removing it

---
+++ 19._Remove_Nth_Node-From_End_of_List.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ListNode* removeNthFromEnd(ListNode* head, int n) {
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    while (n-- > 0)
+      fast = fast->next;
+    if (fast == nullptr)
+      return head->next;
+
+    while (fast->next != nullptr) {
+      slow = slow->next;
+      fast = fast->next;
+    }
+    slow->next = slow->next->next;
+
+    return head;
+  }

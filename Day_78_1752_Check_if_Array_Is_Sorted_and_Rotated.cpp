bdc9a5739d8bff5a5907ca651c0ae47346d53cf5
KartavyaShrivastav <kartavyashrivastav28@gmail.com>
2025-02-02 22:48:35 +0530

    Solved by keeping count of times when arr[i+1] < arr[i], when it exceeds 1 return false

---
+++ Day_78_1752_Check_if_Array_Is_Sorted_and_Rotated.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    bool check(vector<int>& arr) {
+        int count = 0;
+        int n = arr.size();
+        
+        for (int i = 0; i < n; i++) {
+            if (arr[(i + 1) % n] < arr[i]) {
+                count++;
+            }
+            
+            if (count > 1) return false;
+        }
+
+        return true;
+    }
+};

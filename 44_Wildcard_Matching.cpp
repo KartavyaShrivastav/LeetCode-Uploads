38e80883a8b5e07c793ad7f1d5b91e3ad64c5a53
KartavyaShrivastav <kartavyashrivastav28@gmail.com>
2025-02-03 23:24:21 +0530

    Solved by keeping track of the last occurence of the * in the text and treating ? as a skip of one character

---
+++ 44_Wildcard_Matching.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isMatch(string txt, string pat) {
+        int n = txt.length();
+        int m = pat.length();
+        int i = 0, j = 0, startIndex = -1, match = 0;
+
+        while (i < n) {
+            if (j < m && (pat[j] == '?' || pat[j] == txt[i])) {
+                i++;
+                j++;
+            }
+            
+            else if (j < m && pat[j] == '*') {
+                startIndex = j;
+                match = i;
+                j++;
+            }
+        
+            else if (startIndex != -1) {
+                j = startIndex + 1;
+                match++;
+                i = match;
+            }
+            
+            else {        
+                return false;
+            }
+        }
+
+        while (j < m && pat[j] == '*') {
+            j++;
+        }
+        return j == m;
+    }
+};

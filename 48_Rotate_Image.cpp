1c39f3beb224d585e9134221b601b72ad9b0b574
KartavyaShrivastav <kartavyashrivastav28@gmail.com>
2025-02-03 21:27:47 +0530

    Solved by first reversing the rows using ranges::reverse then using double for loop to swap mat[i][j] with mat[j][i]

---
+++ 48_Rotate_Image.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        ranges::reverse(matrix);
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+    }
+};

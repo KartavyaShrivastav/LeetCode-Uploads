25d41a1e4d3bea06a574fb90af4e31eff9fa805c
KartavyaShrivastav <kartavyashrivastav28@gmail.com>
2025-02-04 14:30:04 +0530

    Solved by keeping track of largest sum and current sum in the for loop and then ans is max value of ans and curr

---
+++ Day_80_1800_Maximum_Ascending_Subarray_Sum.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int n = nums.size();
+        int ans = nums[0];
+        int curr = nums[0];
+        for(int i=1; i<n; i++){
+            if(nums[i-1] < nums[i]){
+                curr += nums[i];
+            }
+            else{
+                curr = nums[i];
+            }
+            ans = max(ans, curr);
+        }
+
+        return ans;
+    }
+};

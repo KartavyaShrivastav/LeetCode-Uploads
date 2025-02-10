c0330441a5fab81f52bcc2a2db8825fc163eb69e
KartavyaShrivastav <kartavyashrivastav28@gmail.com>
2025-02-10 23:10:20 +0530

    If character is digit then don't add and also remove the previous character, else add the character to the final answer

---
+++ Day_86_3174_Clear_Digits.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+        string clearDigits(string s) {
+        string ans;
+
+        for (const char c : s)
+        if (isdigit(c))
+            ans.pop_back();
+        else
+            ans += c;
+
+        return ans;
+    }
+};

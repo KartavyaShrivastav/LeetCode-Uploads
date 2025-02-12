16b54dcbb4873a8cff02797953c208f651dfdd57
MayuricaChauhan <iec@mayuricaeducation.in>
2025-02-12 22:59:01 +0530

    Second commit of same code

--- Day_87_1910_Remove_All_Occurrences_of_a_Substring.cpp
+++ Day_87_1910_Remove_All_Occurrences_of_a_Substring.cpp
@@ -11,10 +11,10 @@ public:
         int j = 0;  // t's index
 
         for (int i = 0; i < n; ++i) {
-        t[j++] = s[i];
-        if (j >= k && t.substr(j - k, k) == part)
-            j -= k;
-        }
+            t[j++] = s[i];
+            if (j >= k && t.substr(j - k, k) == part)
+                j -= k;
+            }
 
         return t.substr(0, j);
     }
